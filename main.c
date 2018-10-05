81e1c433293013048ce4ffa3e939a8378b348ef7
dodevs <douglass.sousa@outlook.com.br>
2018-10-05 13:15:34 -0300

    Modificados testes do main

--- main.c
+++ main.c
@@ -5,11 +5,14 @@ int main() {
   lista_t *lista;
   lista = criaLista();
 
-  inserirNoInicio(lista, 5);
-  printf("%d\n", no_getVal(lista_getInic(lista)));
-  inserirNoInicio(lista, 3);
-  printf("%d\n", no_getVal(lista_getInic(lista)));
-  printf("%d\n", no_getVal(no_getProx(lista_getInic(lista))));
-
+  inserirNoInicio(lista, 2);
+  inserirNoFinal(lista, 4);
+  inserirNoInicio(lista, 1);
+  /*(lista_t *lista, int posicao, int valor)*/
+  inserirNaPosicao(lista, 2, 3);
+  inserirNaPosicao(lista, 0, 0);
+  inserirNaPosicao(lista, 5, 5);
+  imprimeLista(lista); /* retorno esperado: 0 <-> 1 <-> 2 <-> 3 <-> 4 <-> 5 */
+  destroiLista(lista);
   return 0;
 }
